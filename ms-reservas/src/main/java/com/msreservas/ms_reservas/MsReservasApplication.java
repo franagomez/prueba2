f090b036114a32b6d1185dd0272d553c443869fb
ghislaine386 <gh.carrasco@duocuc.cl>
2026-07-06 04:09:54 -0400

    Configura Feign y agrega Spring Cloud LoadBalancer

--- ms-reservas/src/main/java/com/msreservas/ms_reservas/MsReservasApplication.java
+++ ms-reservas/src/main/java/com/msreservas/ms_reservas/MsReservasApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableFeignClients
-public class MsReservasApplication {
+public class
+MsReservasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MsReservasApplication.class, args);
 	}
